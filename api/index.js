57ca2af1a74ac68471f707d0a2da8b6865dd86c9
daveliew <daveliew@gmail.com>
2021-07-15 23:44:06 +0800

    testing vercel deployment

--- api/index.js
+++ api/index.js
@@ -1,7 +1,7 @@
 //* DEPENDENCIES
 const express = require("express");
 const methodOverride = require("method-override");
-const Pokemon = require("./models/pokemon");
+const Pokemon = require("../models/pokemon");
 
 //* CONFIGURATION
 require("dotenv").config();
@@ -91,3 +91,17 @@ app.get("/pokemon/:id/edit", (req, res) => {
 app.listen(PORT, () => {
   console.log("Listening on port", PORT);
 });
+
+app.get("/api", (req, res) => {
+  const path = `/api/item/${v4()}`;
+  res.setHeader("Content-Type", "text/html");
+  res.setHeader("Cache-Control", "s-max-age=1, stale-while-revalidate");
+  res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
+});
+
+app.get("/api/item/:slug", (req, res) => {
+  const { slug } = req.params;
+  res.end(`Item: ${slug}`);
+});
+
+module.exports = app;
