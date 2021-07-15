5ebb59088dcfcb0392e012c8ec1bd24f32f55657
daveliew <daveliew@gmail.com>
2021-07-15 23:18:22 +0800

    feat: 🎸 ability to edit pokemon

--- server.js
+++ server.js
@@ -61,20 +61,32 @@ app.delete("/pokemon/:index", (req, res) => {
   res.redirect("/pokemon");
 });
 
-// app.put("/fruits/:id", (req, res) => {
-//   const { id } = req.params;
-//   fruits[id].name = req.body.name;
-//   fruits[id].color = req.body.color;
-//   fruits[id].readyToEat = req.body.readyToEat === "on";
-//   res.redirect("/fruits/");
-// });
+//*edit route
+app.put("/pokemon/:id", (req, res) => {
+  const editPokemonIndex = Pokemon.findIndex((e) => e.id === req.params.id);
+  Pokemon[editPokemonIndex] = {
+    ...Pokemon[editPokemonIndex],
+    name: req.body.name,
+    img: req.body.img,
+    type: req.body.type.split(","),
+    stats: {
+      hp: req.body.hp,
+      attack: req.body.attack,
+      defense: req.body.defense,
+      spattack: req.body.spattack,
+      spdefense: req.body.spdefense,
+      speed: req.body.speed,
+    },
+  };
+  console.log(Pokemon[editPokemonIndex]);
+  res.redirect("/pokemon/");
+});
 
-// //*edit route
-// app.get("/fruits/:id/edit", (req, res) => {
-//   const { id } = req.params;
-//   const fruit = fruits[id];
-//   res.render("edit.ejs", { fruit, id });
-// });
+app.get("/pokemon/:id/edit", (req, res) => {
+  const pos = req.params.id;
+  const pokemon = Pokemon.filter((pokemon) => pokemon.id === pos)[0];
+  res.render("edit.ejs", { pokemon, pos });
+});
 
 app.listen(PORT, () => {
   console.log("Listening on port", PORT);
